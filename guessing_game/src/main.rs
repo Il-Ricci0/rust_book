d7a50e7c6b00dec3c2a53a1a4ceb667efb077322
Il-Ricci0 <riccardoroman2004@gmail.com>
2026-03-23 23:56:04 +0100

    Added random number to guessing_game.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,12 @@
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Shhhhhh! the secret number is {secret_number}");
 
     println!("Please input your guess.");
 
@@ -9,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You are correct! that was a good guess :)"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
